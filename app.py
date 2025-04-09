436c1618c55d844ab94dab8b2330a97737a4b7e8
PeaceOfMaster <kirillmaster2020@gmail.com>
2025-04-10 00:31:38 +0300

    Root Application Dispatching

---
+++ app.py
@@ -0,0 +1,11 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from lab1.app import app as lab1_app
+from lab2.app import app as lab2_app
+from lab3.app import app as lab3_app
+from root_app.app import app as root_app
+
+application = DispatcherMiddleware(root_app, {
+    '/lab1': lab1_app,
+    '/lab2': lab2_app,
+    '/lab3': lab3_app
+})
