cb0b0c50ce1822ddfa0ab97b6f2eaec264376b92
PeaceOfMaster <kirillmaster2020@gmail.com>
2025-04-13 22:44:56 +0300

    Lab4 db.py disconnect feature

--- lab4/app/db.py
+++ lab4/app/db.py
@@ -10,15 +10,20 @@ class DBConnector:
         self.app = app
         self.app.teardown_appcontext(self.disconnect)
 
-def _get_config(self):
-    return {
-        'user': self.app.config['MYSQL_USER'],
-        'password': self.app.config['MYSQL_PASSWORD'],
-        'host': self.app.config['MYSQL_HOST'],
-        'database': self.app.config['MYSQL_DATABASE']
-    }
+    def _get_config(self):
+        return {
+            'user': self.app.config['MYSQL_USER'],
+            'password': self.app.config['MYSQL_PASSWORD'],
+            'host': self.app.config['MYSQL_HOST'],
+            'database': self.app.config['MYSQL_DATABASE']
+        }
 
-def connect(self):
-    if 'db' not in g:
-        g.db = mysql.connector.connect(**self._get_config())
-    return g.db
+    def connect(self):
+        if 'db' not in g:
+            g.db = mysql.connector.connect(**self._get_config())
+        return g.db
+
+    def disconnect(self, e=None):
+        if 'db' in g:
+            g.db.close()
+        g.pop('db', None)
